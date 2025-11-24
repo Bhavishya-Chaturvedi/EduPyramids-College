b98699ea38730f1cc567a0ea15cc7835b08249b8
Naman Sharma <namansharma567891@gmail.com>
2025-11-24 16:10:21 +0000

    fetched the payment status file from a2 branch

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,13 @@
 
 import { Routes, Route } from "react-router-dom";
+import ResponsiveAppBar from "./components/homepage/ResponsiveAppBar";
 import MegaMenu from "./components/homepage/AppBar";
 // import FeatureTiles from "./components/homepage/HomeComponents";
 import HomePage from "./pages/home/Homepage";
 import DomainPage from "./pages/public/DomainsPage";
 import CoursePage from "./pages/public/CoursePage";
 import TutorialSearch from "./pages/public/TutorialSearch";
-import SubscriptionPage from "./pages/public/SubscriptionPage";
-import LoginPage from "./features/auth/pages/LoginPage";
-import DashboardLayout from "./features/dashboard/pages/DashboardLayout";
-import PublicLayout from "./pages/public/PublicLayout";
-import Dashboard from "./features/dashboard/pages/Dashboard";
-import TrainingPlanner from "./features/training/pages/TrainingPlanner";
-import TrainingAttendance from "./features/training/pages/TrainingAttendance";
+import PaymentStatus from "./pages/public/PaymentStatus";
 
 
 export default function App(){
@@ -20,28 +15,15 @@ export default function App(){
   return (
     <>
         {/* <ResponsiveAppBar/> */}
-        {/* <MegaMenu/> */}
+        <MegaMenu/>
         {/* <HomePage /> */}
-        
+        {/* Define the routes */}
         <Routes>
-          {/* Public routes */}
-          <Route element={<PublicLayout />}>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/domains" element={<DomainPage />} />
-            <Route path="/tutorial-search" element={<TutorialSearch />} />
-            <Route path="/domains/:slug" element={<CoursePage />} />
-            <Route path="/subscription" element={<SubscriptionPage />} />
-          </Route>
-
-         {/* Dashboard routes */}
-           <Route path="dashboard" element={<DashboardLayout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="training-planner" element={<TrainingPlanner />} />
-            <Route path="training-attendance" element={<TrainingAttendance />} />
-            {/* <Route path="academic-subscription" element={<AcademicSubscription />} /> */}
-          </Route>
-
+          <Route path="/" element={<HomePage />} />
+          <Route path="/domains" element={<DomainPage />} />
+          <Route path="/tutorial-search" element={<TutorialSearch />} />
+          <Route path="/domains/:slug" element={<CoursePage />} />
+          <Route path="/payement-status" element={<PaymentStatus />} />
           {/* catch-all for 404 */}
           <Route path="*" element={<h1>Page Not Found</h1>} />
         </Routes>
